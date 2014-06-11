9204f24067acead0e36c85d315bf9b0b4c1d4d6c
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-06-11 13:43:07 -0700

    Add YARD

--- lib/hash_selectors.rb
+++ lib/hash_selectors.rb
@@ -1,9 +1,21 @@
+# Provides additional select methods for Hashes.
+# Automatically mixed-in to Hash on load.
 module HashSelectors
 
+  # @example
+  #   {a: 1, b: 2, c: 3}.select_by_keys :a, :b # returns {a: 1, b: 2}
+  #
+  # @param [Glob of any type] ks
+  # @return [Hash] That subset of the Hash whose keys are included within the *ks* argument.
   def select_by_keys(*ks)
     select { |k,v| ks.include?(k) }
   end
 
+  # @example
+  #   {a: 1, b: 2, c: 3}.select_by_values 2, 3 # returns {b: 2, c: 3}
+  #
+  # @param [Glob of any type] vs
+  # @return [Hash] That subset of the Hash whose values are included within the *vs* argument.
   def select_by_values(*vs)
     select { |k,v| vs.include?(v) }
   end
