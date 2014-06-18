498e6f796a4fd8ed3fa7778f0398c0fc1332c439
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-06-18 08:37:53 -0700

    Add reject_by_keys and reject_by_values

--- lib/hash_selectors.rb
+++ lib/hash_selectors.rb
@@ -3,6 +3,24 @@
 module HashSelectors
 
   # @example
+  #   {a: 1, b: 2, c: 3}.reject_by_keys :a, :b # returns {a: 1, b: 2}
+  #
+  # @param [Glob of any type] ks
+  # @return [Hash] That subset of the Hash whose keys are included within the *ks* argument.
+  def reject_by_keys(*ks)
+    reject { |k,v| ks.include?(k) }
+  end
+
+  # @example
+  #   {a: 1, b: 2, c: 3}.reject_by_values 2, 3 # returns {b: 2, c: 3}
+  #
+  # @param [Glob of any type] vs
+  # @return [Hash] That subset of the Hash whose values are included within the *vs* argument.
+  def reject_by_values(*vs)
+    reject { |k,v| vs.include?(v) }
+  end
+
+  # @example
   #   {a: 1, b: 2, c: 3}.select_by_keys :a, :b # returns {a: 1, b: 2}
   #
   # @param [Glob of any type] ks
--- spec/hash_selectors_spec.rb
+++ spec/hash_selectors_spec.rb
@@ -7,6 +7,34 @@ RSpec.describe HashSelectors do
 
   ### INSTANCE METHODS
 
+  describe "#reject_by_keys" do
+    context "assuming a Hash: {a: 1, b: 2, c: {c2: [:d, :e]}}" do
+      let(:the_hash) { {a: 1, b: 2, c: {c2: [:d, :e]}} }
+      context "and given :a, :d" do
+        subject { the_hash.reject_by_keys :a, :d }
+        it { is_expected.to eq({b: 2, c: {c2: [:d, :e]}}) }
+      end
+      context "and given :a, :b" do
+        subject { the_hash.reject_by_keys :a, :b }
+        it { is_expected.to eq({c: {c2: [:d, :e]}}) }
+      end
+      context "and given :b, :c" do
+        subject { the_hash.reject_by_keys :b, :c }
+        it { is_expected.to eq({a: 1}) }
+      end
+    end
+  end
+
+  describe "#reject_by_values" do
+    context "assuming a Hash: {a: 1, b: 2, c: {c2: [:d, :e]}}" do
+      let(:the_hash) { {a: 1, b: 2, c: {c2: [:d, :e]}} }
+      context "and given 1, 2" do
+        subject { the_hash.reject_by_values 1, 2 }
+        it { is_expected.to eq({c: {c2: [:d, :e]}}) }
+      end
+    end
+  end
+
   describe "#select_by_keys" do
     context "assuming a Hash: {a: 1, b: 2, c: {c2: [:d, :e]}}" do
       let(:the_hash) { {a: 1, b: 2, c: {c2: [:d, :e]}} }
