587987881b85cf6524db62eab146661d7376af02
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-06-18 09:05:53 -0700

    Add partition_by... methods and fix YARD for reject_by...

--- lib/hash_selectors.rb
+++ lib/hash_selectors.rb
@@ -3,19 +3,41 @@
 module HashSelectors
 
   # @example
-  #   {a: 1, b: 2, c: 3}.reject_by_keys :a, :b # returns {a: 1, b: 2}
+  #   {a: 1, b: 2, c: 3}.partition_by_keys :a, :b # returns [{a: 1, b: 2}, {c: 3}]
   #
   # @param [Glob of any type] ks
-  # @return [Hash] That subset of the Hash whose keys are included within the *ks* argument.
+  # @return [Hash] Partitioned results based on whether keys are included within the *ks* argument.
+  def partition_by_keys(*ks)
+    ### OPTIMIZE: Simple partition with the block results in nested Arrays. Investigate.
+    blk = lambda { |k,v| ks.include?(k) }
+    [select(&blk), reject(&blk)]
+  end
+
+  # @example
+  #   {a: 1, b: 2, c: 3}.partition_by_values 2, 3 # returns [{b: 2, c: 3}, {a: 1}]
+  #
+  # @param [Glob of any type] vs
+  # @return [Hash] Partitioned results based on whether values are included within the *vs* argument.
+  def partition_by_values(*vs)
+    ### OPTIMIZE: Simple partition with the block results in nested Arrays. Investigate.
+    blk = lambda { |k,v| vs.include?(v) }
+    [select(&blk), reject(&blk)]
+  end
+
+  # @example
+  #   {a: 1, b: 2, c: 3}.reject_by_keys :a, :b # returns {c: 3}
+  #
+  # @param [Glob of any type] ks
+  # @return [Hash] That subset of the Hash whose keys are not found within the *ks* argument.
   def reject_by_keys(*ks)
     reject { |k,v| ks.include?(k) }
   end
 
   # @example
-  #   {a: 1, b: 2, c: 3}.reject_by_values 2, 3 # returns {b: 2, c: 3}
+  #   {a: 1, b: 2, c: 3}.reject_by_values 2, 3 # returns {a: 1}
   #
   # @param [Glob of any type] vs
-  # @return [Hash] That subset of the Hash whose values are included within the *vs* argument.
+  # @return [Hash] That subset of the Hash whose values are not found within the *vs* argument.
   def reject_by_values(*vs)
     reject { |k,v| vs.include?(v) }
   end
